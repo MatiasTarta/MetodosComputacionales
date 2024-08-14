b8316c141047848f3060a068497145860bd79946
matiastarta <matias.tartaglia@est.fi.uncoma.edu.ar>
2024-08-14 15:37:49 -0300

    integracion Numerica lista

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         float h, n, x0, xn,res;
-
+        
         // Solicitar datos al usuario
         System.out.println("Ingrese su H:");
         h = sc.nextFloat();
@@ -20,7 +20,6 @@ public class App {
 
         // Crear la matriz
         float[][] matriz = new float[2][(int) n];
-
         // Llenar la primera fila con los valores de x
         for (int i = 0; i < n; i++) {
             matriz[0][i] = x0 + i * h;
@@ -32,33 +31,52 @@ public class App {
             System.out.print("f(" + matriz[0][i] + "): ");
             matriz[1][i] = sc.nextFloat();
         }
+
         // Imprimir la tabla de datos
         System.out.println("\nTabla de datos:");
         System.out.println("x\tf(x)");
         for (int i = 0; i < n; i++) {
             System.out.println(matriz[0][i] + "\t" + matriz[1][i]);
         }
-        System.out.println("Elija su metodo de aproximacion de Area");
+
+        while(true){
+            System.out.println("Elija su metodo de aproximacion de Area");
         System.out.println("PARA METODO DE LOS TRAPECIOS DIGITE 1");
         System.out.println("PARA METODO DE SIMPSON 1/3 DIGITE 2");
         int num =sc.nextInt();
         if(num==1){
-            res= trapecio(matriz,h,n);
+            res= trapecio(matriz,n,h);
             System.out.println("El area de la funcion en base a la tabla es de: "+res);
         }else if(num==2){
-            
+            res= simpson13(matriz, num, h);
+            System.out.println("El area de la funcion en base a la tabla es de: "+res);
         }else{
             System.out.println("!ERROR DIGITO INVALIDO¡");
+        }
+         
         }
     }
 
     public static float trapecio(float[][] matriz,float n, float h){
         float suma=0,resultado=0;
-
-        for(int i=1;i<n;i++){
+          for(int i=1;i<n-1;i++){
             suma= matriz[1][i]+suma;
         }
-        resultado= h/2 * (matriz[1][0]+ matriz[1][(int) n] + 2*suma);
+        resultado= h/2 * (matriz[1][0]+ matriz[1][(int) n-1] + 2*suma);
+        return resultado;
+        
+    }
+
+    public static float simpson13(float[][] matriz,float n, float h){
+        float sumaPar=0,sumaImpar=0,resultado=0;
+          for(int i=1;i<n-1;i++){
+            if(i%2==0){
+                sumaPar= matriz[1][i]+sumaPar;
+            }else{
+                sumaImpar= matriz[1][i]+sumaImpar;
+            }
+        }
+        resultado= h/3 * (matriz[1][0]+ matriz[1][(int) n-1] + 2*sumaPar+4*sumaImpar);
         return resultado;
         
     }
